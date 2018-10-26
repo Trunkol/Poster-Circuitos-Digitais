5d935603f51a95f60fb8011be870698c094dc1fc
Emerson Costa <costtasemerson@gmail.com>
2018-10-26 17:33:54 -0300

    Adicionando funcionamento

--- atv2.vhdl
+++ atv2.vhdl
@@ -1,17 +1,24 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity atv2 is
+entity projeto is
 	port(
 		clockcont, s, enable, reset : in std_logic;
 		qc : out std_logic_vector(2 downto 0);
 		d : out std_logic_vector(6 downto 0)
 	);
-end atv2;
+end projeto;
 
+
+architecture projeto of projeto is
+	
 component DivisorFrequencia is
 	port( 
 		clock_in : in std_logic;
 		clock_out : out std_logic
 	);
 end component;
+
+begin
+	
+end projeto;
