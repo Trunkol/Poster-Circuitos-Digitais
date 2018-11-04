b122167ff96bc9b3e3ce3af9201f172885eed48b
Emerson Costa <emerson.silva@lais.huol.ufrn.br>
2018-11-04 17:37:50 -0300

    corrigindo erros nos switchs

--- contador.vhdl
+++ contador.vhdl
@@ -18,6 +18,13 @@ signal J3,J4,J5,J6,Q1,Q2,Q3,Q4,Q5,Q6,Qbar1,Qbar2,Qbar3,Qbar4, Qbar5, Qbar6 : std
 signal acaiverde, acaiamarelo, acaivermelho, guaranaverde, guaranamarelo, guaranavermelho : std_logic;
 signal sita, sitb, sitc : std_logic;
 
+signal sita_acai_verde : std_logic;
+signal sita_acai_amarelo : std_logic;
+signal sita_acai_vermelho : std_logic;
+signal sita_guarana_verde : std_logic;
+signal sita_guarana_amarelo : std_logic;
+signal sita_guaranda_vermelho : std_logic;
+
 begin 
 	J3 <= Q1 and Q2;
 	J4<= J3 and Q3;
@@ -44,60 +51,61 @@ begin
 	-- F = Q1
 	
 	--situação A:
-	  -- y = A'B' + A'C'D'
-		acaiverde <= sita and ((not Q6 and not Q5) or (not(Q6) and not Q4 and not Q3));
-	 
-	  -- y = A'BC'DE' + A'BC'DF'
-	  acaiamarelo <= sita and ((not Q6 and Q5 and not Q4 and Q3 and not Q2) or (not Q6 and Q5 and not Q4 and Q3 and not Q1));
-	
-	  -- y = A'BC + AB'C' + AB'D' + AB'E' + AB'F' + A'BDEF
-	  acaivermelho <= sita and ((not Q6 and Q5 and Q4) or 
-							(Q6 and not Q5 and not Q4) or 
-							(Q6 and (not Q5) and not Q3) or 
-							(Q6 and (not Q5) and (not Q1)) or 
-							(not (Q6) and (Q5 and Q3) and (Q2 and Q1)));
-	 
-	 -- y = A'BC + AB'C' + AB'D'E' + AB'D'F' + A'BDEF
-	  guaranaverde <= sita and ((not Q6 and Q5 and Q4) or 
-							(Q6 and not Q5 and not Q4) or 
-							(Q6 and not Q5 and not Q3 and not Q2) or 
-							(Q6 and not Q5 and not Q3 and not Q1)or 
-							(not Q6 and Q5 and Q3 and Q2 and Q1));
-	
-	-- y = AB'CDE' + AB'CD'EF
-	  guaranamarelo <= sita and ((Q6 and not Q5 and Q4 and Q3 and not Q2) or 
-										  (Q6 and not Q5 and Q4 and Q3 and Q2 and Q1));
-	  guaranavermelho <= sita and (acaiverde or acaiamarelo);
-
-	--situação B:
+	acaiverde <= 
+		-- y = A'B' + A'C'D' (0 a 19)
+		(((not Q6 and not Q5) or (not(Q6) and not Q4 and not Q3)) and sita) or 
 		--  y = A'B' + A'C' + A'D' + A'E' (0 a 29)
-			acaiverde <= sitb and ((not Q6 and not Q5) or 
-							 (not Q6 and not Q4) or 
-							 (not Q6 and not Q3) or 
-							 (not Q6 and not Q2));
-							 
+		(((not Q6 and not Q5) or (not Q6 and not Q4) or (not Q6 and not Q3) or (not Q6 and not Q2)) and sitb);
+	 
+	acaiamarelo <= 
+		-- y = A'BC'DE' + A'BC'DF' (20 a 22)
+		(((not Q6 and Q5 and not Q4 and Q3 and not Q2) or (not Q6 and Q5 and not Q4 and Q3 and not Q1)) and sita) or
 		-- y = A'BCDE + AB'C'D'E'F' (30 a 32)
-			acaiamarelo <= sitb and ((not Q6 and Q5 and Q4 and Q3 and Q2) or 
-								(Q6 and not Q5 and not Q4 and not Q3 and not Q2 and not Q1));
-		
+		(((not Q6 and Q5 and Q4 and Q3 and Q2) or (Q6 and not Q5 and not Q4 and not Q3 and not Q2 and not Q1)) and sitb);
+ 
+	acaivermelho <= 
+		-- y = A'BC + AB'C' + AB'D' + AB'E' + AB'F' + A'BDEF
+		 ((
+			(not Q6 and Q5 and Q4) or 
+			(Q6 and not Q5 and not Q4) or 
+			(Q6 and (not Q5) and not Q3) or 
+			(Q6 and (not Q5) and (not Q1)) or 
+			(not (Q6) and (Q5 and Q3) and (Q2 and Q1))) 
+			and sita) 
+			or
 		--	y = AB'C'F + AB'C'E + AB'DE' + AB'CD'
-			acaivermelho <= sitb and ((Q6 and not Q5 and not Q4 and Q1) or 
-								 (Q6 and not Q5 and not Q4 and Q2) or 
-								 (Q6 and not Q5 and Q3 and not Q2) or 
-								 (Q6 and not Q5 and Q4 and not Q3));
+		 ((
+			(Q6 and not Q5 and not Q4 and Q1) or 
+			(Q6 and not Q5 and not Q4 and Q2) or 
+			(Q6 and not Q5 and Q3 and not Q2) or 
+			(Q6 and not Q5 and Q4 and not Q3)) 
+			and sitb);
 		
-		-- y = y = AB'C'F + AB'C'D + AB'D'EF' + AB'CD'E'
-			guaranaverde <= sitb and ((Q6 and not Q5 and not Q4 and Q1) or 
-								 (Q6 and not Q5 and not Q4 and Q3) or 
-								 (Q6 and not Q5 and not Q3 and Q2 and not Q1) or 
-								 (Q6 and not Q5 and Q4 and not Q3 and not Q2));
-		
+	 
+	  guaranaverde <= 
+	 -- y = A'BC + AB'C' + AB'D'E' + AB'D'F' + A'BDEF 
+	 ((
+		(not Q6 and Q5 and Q4) or (Q6 and not Q5 and not Q4) or 
+		(Q6 and not Q5 and not Q3 and not Q2) or 
+		(Q6 and not Q5 and not Q3 and not Q1) or 
+		(not Q6 and Q5 and Q3 and Q2 and Q1)) 
+		and sita) or
+	 -- y = y = AB'C'F + AB'C'D + AB'D'EF' + AB'CD'E'
+	 (((Q6 and not Q5 and not Q4 and Q1) or 
+		(Q6 and not Q5 and not Q4 and Q3) or 
+		(Q6 and not Q5 and not Q3 and Q2 and not Q1) or 
+		(Q6 and not Q5 and Q4 and not Q3 and not Q2)) and sitb);
+
+	
+	  guaranamarelo <= 
+	  -- y = AB'CDE' + AB'CD'EF
+	  (((Q6 and not Q5 and Q4 and Q3 and not Q2) or (Q6 and not Q5 and Q4 and Q3 and Q2 and Q1)) and sita)  or
 		-- y = AB'CDE' + AB'CD'EF
-			guaranamarelo <= sitb and ((Q6 and not Q5 and Q4 and Q3 and not Q2) or 
-								  (Q6 and not Q5 and Q4 and not Q3 and Q2 and Q1));
-		
-			guaranavermelho <= sitb and (acaiverde or acaiamarelo);
---  
+	  (((Q6 and not Q5 and Q4 and Q3 and not Q2) or (Q6 and not Q5 and Q4 and not Q3 and Q2 and Q1)) and sitb);
+	  
+	  guaranavermelho <= acaiverde or acaiamarelo;
+
+----  
 --  --situação C:
 --  ave <= '0';
 --  aam <= not Q1;
