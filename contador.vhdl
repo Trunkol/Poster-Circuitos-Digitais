e555262ed92adf1926e0042ff7c7631f077d5085
Emerson Costa <emerson.silva@lais.huol.ufrn.br>
2018-11-04 14:41:10 -0300

    contador com as expressões lógicas adicionais. Falta adicionar a condição para chavear entre qual situação isso vai ficar

--- contador.vhdl
+++ contador.vhdl
@@ -6,7 +6,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity contador is
 port ( clk:     in std_logic;
           reset:      in std_logic;
-          counter : out std_logic_vector(5 downto 0)
+          counter, sinais : out std_logic_vector(5 downto 0)
 );
 end contador;
 
@@ -14,7 +14,8 @@ end contador;
 architecture Behavioral of contador is
 --signal declaration.
 signal J3,J4,J5,J6,Q1,Q2,Q3,Q4,Q5,Q6,Qbar1,Qbar2,Qbar3,Qbar4, Qbar5, Qbar6 : std_logic :='0';
-signal ave, aam, avo, gve, gam, gvo : std_logic;
+signal acaiverde, acaiamarelo, acaivermelho, guaranaverde, guaranamarelo, guaranavermelho : std_logic;
+signal contador : std_logic_vector(5 downto 0);
 
 begin 
 	J3 <= Q1 and Q2;
@@ -31,21 +32,38 @@ begin
 	FF5 : entity work.flipflopJK port map (clk,J5,J5,Q5,Qbar5,reset);
 	FF6 : entity work.flipflopJK port map (clk,J6,J6,Q6,Qbar6,reset);
 	
+	-- A = Q6
+	-- B = Q5
+	-- C = Q4
+	-- D = Q3
+	-- E = Q2
+	-- F = Q1
+	
 	--situação A:
-  ave <= not(Q6 and Q5) or (not(Q6) and Q4 and Q3);
-  aam <= (Q5 and not Q4 and Q3 and not Q2) or (Q5 and not Q4 and Q3 and not Q1);
-  avo <= Q6 or (Q5 and Q4) or ((Q5 and Q3) and (Q2 and Q1));
-  gve <= (Q5 and Q4) or (Q6 and not Q4) or (Q6 and not Q3 and not Q2) or (Q6 and not Q3 and not Q1) or (Q5 and Q3 and Q2 and Q1);
-  gam <= (Q6 and Q4 and Q3) or (Q6 and Q4 and Q2 and Q1);
-  gvo <= (not Q6 and not Q5) or not(Q6 and Q4 and Q3) or not(Q6 and Q4 and Q2) or not(Q6 and Q4 and Q1);
+	  -- y = A'B' + A'C'D'
+	  --acaiverde <= (not Q6 and not Q5) or (not(Q6) and not Q4 and not Q3);
+	  -- y = A'BC'DE' + A'BC'DF'
+	  --acaiamarelo <= (not Q6 and Q5 and not Q4 and Q3 and not Q2) or (not Q6 and Q5 and not Q4 and Q3 and not Q1);
+	  -- y = A'BC + AB'C' + AB'D' + AB'E' + AB'F' + A'BDEF
+	  --acaivermelho <= (not Q6 and Q5 and Q4) or (Q6 and not Q5 and not Q4) or (Q6 and (not Q5) and not Q3) or (Q6 and (not Q5) and (not Q1)) or (not (Q6) and (Q5 and Q3) and (Q2 and Q1));
+	  -- y = A'BC + AB'C' + AB'D'E' + AB'D'F' + A'BDEF
+	  --guaranaverde <= (not Q6 and Q5 and Q4) or (Q6 and not Q5 and not Q4) or (Q6 and not Q5 and not Q3 and not Q2) or (Q6 and not Q5 and not Q3 and not Q1)or (not Q6 and Q5 and Q3 and Q2 and Q1);
+	  -- y = AB'CDE' + AB'CD'EF
+	  --guaranamarelo <= (Q6 and not Q5 and Q4 and Q3 and not Q2) or (Q6 and not Q5 and Q4 and Q3 and Q2 and Q1);
+	  --guaranavermelho <= ave or aam;
 
---  --situação B:
---  ave <= not(Q6 and Q5) or not(Q6 and Q4) or not(Q6 and Q3) or not(Q6 and Q2);
---  aam <= (Q5 and Q4 and Q3 and Q2) or (Q6 and not Q4 and not Q3 and not Q2 and not Q1);
---  avo <= (Q6 and Q1) or (Q6 and Q2) or (Q6 and Q3) or (Q6 and Q4);
---  gve <= (Q6 and not Q4 and Q1) or (Q6 and Q2 and not Q1) or (Q6 and not Q4 and Q3) or (Q6 and Q4 and not Q3 and not Q2);
---  gam <= (Q6 and Q4 and Q3) or (Q6 and Q4 and Q2 and Q1);
---  gvo <= not Q6 or not(Q4 and Q3 and Q2 and Q1);
+	--situação B:
+		--  y = A'B' + A'C' + A'D' + A'E' (0 a 29)
+			acaiverde <= (not Q6 and not Q5) or (not Q6 and not Q4) or (not Q6 and not Q3) or (not Q6 and not Q2);
+		-- y = A'BCDE + AB'C'D'E'F' (30 a 32)
+			acaiamarelo <= (not Q6 and Q5 and Q4 and Q3 and Q2) or (Q6 and not Q5 and not Q4 and not Q3 and not Q2 and not Q1);
+		--	y = AB'C'F + AB'C'E + AB'DE' + AB'CD'
+			acaivermelho <= (Q6 and not Q5 and not Q4 and Q1) or (Q6 and not Q5 and not Q4 and Q2) or (Q6 and not Q5 and Q3 and not Q2) or (Q6 and not Q5 and Q4 and not Q3);
+		-- y = y = AB'C'F + AB'C'D + AB'D'EF' + AB'CD'E'
+			guaranaverde <= (Q6 and not Q5 and not Q4 and Q1) or (Q6 and not Q5 and not Q4 and Q3) or (Q6 and not Q5 and not Q3 and Q2 and not Q1) or (Q6 and not Q5 and Q4 and not Q3 and not Q2);
+		-- y = AB'CDE' + AB'CD'EF
+			guaranamarelo <= (Q6 and not Q5 and Q4 and Q3 and not Q2) or (Q6 and not Q5 and Q4 and not Q3 and Q2 and Q1);
+		   guaranavermelho <= acaiverde or acaiamarelo;
 --  
 --  --situação C:
 --  ave <= '0';
@@ -54,8 +72,8 @@ begin
 --  gve <= '0';
 --  gam <= not Q1;
 --  gvo <= '0';
-	
-	
-	counter <= Q6 & Q5 & Q4 & Q3 & Q2 & Q1; 
+	sinais <= acaiverde & acaiamarelo & acaivermelho & guaranaverde & guaranamarelo & guaranavermelho; 
+	counter <= Q6 & Q5 & Q4 & Q3 & Q2 & Q1;
+
 
 end Behavioral;
